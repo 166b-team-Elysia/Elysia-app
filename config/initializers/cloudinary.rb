bb31a91fcf5557239b2d4c71239269abab07cb3a
Zhendong Yin <zhendongyin@brandeis.edu>
2021-12-14 00:22:05 -0500

    fix

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = "hnbpq9ihb"
+  config.api_key = "771294181191232"
+  config.api_secret = "TbwxAU980bAW_WufcNY04S6rjjQ"
+  config.secure = true
+  config.cdn_subdomain = true
+end
