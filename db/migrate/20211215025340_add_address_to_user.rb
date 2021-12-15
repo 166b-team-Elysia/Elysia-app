abc8bfa0eb32eb40985d4d8d08d4779432beb956
Zhendong Yin <zhendongyin@brandeis.edu>
2021-12-14 21:56:38 -0500

    fix

---
+++ db/migrate/20211215025340_add_address_to_user.rb
@@ -0,0 +1,11 @@
+class AddAddressToUser < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :address, :string
+    add_column :users, :city_id, :integer, index: true
+    add_column :users, :state_id, :integer, index: true
+    add_column :users, :postal_code, :integer, index: true
+    add_column :users, :longitude, :float
+    add_column :users, :latitude, :float
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_14_232128) do
+ActiveRecord::Schema.define(version: 2021_12_15_025340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -177,6 +177,12 @@ ActiveRecord::Schema.define(version: 2021_12_14_232128) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "role", default: 0
+    t.string "address"
+    t.integer "city_id"
+    t.integer "state_id"
+    t.integer "postal_code"
+    t.float "longitude"
+    t.float "latitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
   end
